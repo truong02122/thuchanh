2fdfb527d111b063913de6517a597ce406b1ad13
truong02122 <97438169+truong02122@users.noreply.github.com>
2022-05-09 19:06:52 +0700

    VoVanTruong

--- Bai thuc hanh 9-5/src/app17.java
+++ Bai thuc hanh 9-5/src/app17.java
@@ -1,22 +1,18 @@
 import java.util.ArrayList;
 
-public class app17{
-	public static void main(String[] args){
-	    // khai báo1ArrayList có tên là arrlistString
-	   // có kiểu là String
-	    ArrayList<Integer> arrlistInteger=new ArrayList<>();
-	   // thêm các phần tử sử dụng phương thức add()
-	    arrlistInteger.add(0);
-	    arrlistInteger.add(7);
-	    arrlistInteger.add(1);
-	    arrlistInteger.add(9);
-	   // duyệt theo kích thước của arrListInteger
-	   // sử dụng vòng lặp for duyệt theo đối tượng
-	   // trong đó kiều dữ liệu của biến number
-	   // phải trùng với kiểu dữ liệu của arrlistInteger
-	    System.out.println("Các phần tử có trong arrListInteger là: ");
-	for (int number : arrlistInteger){
-	    System.out.print(number + "\t");
-	}
+public class app17 {
+	public static void main(String[] args) {
+
+		ArrayList<Integer> arrlistInteger = new ArrayList<>();
+
+		arrlistInteger.add(0);
+		arrlistInteger.add(7);
+		arrlistInteger.add(1);
+		arrlistInteger.add(9);
+
+		System.out.println("Các phần tử có trong arrListInteger là: ");
+		for (int number : arrlistInteger) {
+			System.out.print(number + "\t");
+		}
 	}
 }
